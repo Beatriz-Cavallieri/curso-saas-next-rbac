c4ab6cb862ba0cac3706f2e4e7f6429811041713
Beatriz Cavallieri <bfcavallieri@gmail.com>
2025-03-09 08:05:38 -0300

    Creates permissions setup

---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,18 @@
+import { AbilityBuilder } from "@casl/ability"
+import { AppAbility } from "."
+
+export type Role = 'ADMIN' | 'MEMBER'
+
+type PermissionsByRole = (
+    user: unknown,
+    builder: AbilityBuilder<AppAbility>
+) => void
+
+export const permissions: Record<Role, PermissionsByRole> = {
+    ADMIN(_, { can }) {
+        can('manage', 'all')
+    },
+    MEMBER(_, { can }) {
+        can('invite', 'User')
+    }
+}
